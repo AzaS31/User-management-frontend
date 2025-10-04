5019522a9dd5712e5bd5ddc86f11b2f8a0cee2ca
Azamat <azamatsalaly@gmail.com>
2025-10-05 01:34:51 +0500

    prod

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
@@ -9,8 +9,8 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response?.status === 403 || error.response?.status === 401) {
-            localStorage.removeItem('user');
-            window.location.href = '/login';
+            localStorage.removeItem('user'); 
+            window.location.href = '/login'; 
         }
         return Promise.reject(error);
     }
